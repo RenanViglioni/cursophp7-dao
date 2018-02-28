a511411641878139111b21671d7112859b205815
Renan Viglioni <renan.santosv@gmail.com>
2018-02-28 16:52:18 -0300

    Modificamos a classe usuário

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+/*$root = new Usuario();
 
 $root->loadById(3);
 
-echo $root;
+echo $root;*/
+
+/*$lista = Usuario::getList();
+
+echo json_encode($lista);*/
+
+ /*$search = Usuario::search("ro");
+
+ echo json_encode($search);*/
+
+ $usuario = new Usuario();
+
+ $usuario->login("root","root");
+
+ echo $usuario;
 
 ?>
